455668d3808112c2d574d011f3b0737bc58ea49a
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-07-06 11:53:38 +0200

    added supabase

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script setup>
+const client = useSupabaseClient();
+
+onMounted(async () => {
+  let { data: bilanzen, error } = await client.from("bilanzen").select("*");
+  console.log(bilanzen);
+  console.log("hello");
+});
+</script>
+
 <template>
   <button class="btn btn-active text-red-900">Default</button>
   <button class="btn btn-active btn-neutral">Neutral</button>
