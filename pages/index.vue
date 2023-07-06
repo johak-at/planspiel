c93e94afd19de41977bf6569fc59432f2a50b31e
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-07-06 11:35:17 +0200

    added index.vue

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="btn btn-active text-red-900">Default</button>
+  <button class="btn btn-active btn-neutral">Neutral</button>
+  <button class="btn btn-active btn-primary">Primary</button>
+  <button class="btn btn-active btn-secondary">Secondary</button>
+  <button class="btn btn-active btn-accent">Accent</button>
+  <button class="btn btn-active btn-ghost">Ghost</button>
+  <button class="btn btn-active btn-link">Link</button>
+</template>
