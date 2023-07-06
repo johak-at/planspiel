02b2f4c17ad9d8b7b5acce0c9250a29d214de482
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-07-06 11:29:23 +0200

    added tailwind & daisy.ui

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button class="btn btn-active text-red-800">Default</button>
+    <button class="btn btn-active btn-neutral">Neutral</button>
+    <button class="btn btn-active btn-primary">Primary</button>
+    <button class="btn btn-active btn-secondary">Secondary</button>
+    <button class="btn btn-active btn-accent">Accent</button>
+    <button class="btn btn-active btn-ghost">Ghost</button>
+    <button class="btn btn-active btn-link">Link</button>
   </div>
 </template>
