7c333daf149c2fbd7a701b4de2bfc47472a353b3
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-07-08 15:32:38 +0200

    added module to persist pinia to local storage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+  ],
+  piniaPersistedstate: {
+    storage: "localStorage",
+  },
 });
