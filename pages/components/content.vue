3f610736b25668fc51494306c9c212fc9b61354a
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2023-09-08 10:32:03 +0200

    links und rechtsbündigkeit der € Zeichen und Betrag

--- pages/components/content.vue
+++ pages/components/content.vue
@@ -1,78 +1,88 @@
 <script setup>
-    import { useStore } from "~/store/store";
-    import { storeToRefs } from "pinia";
+import { useStore } from "~/store/store";
+import { storeToRefs } from "pinia";
 
-    const store = useStore();
+const store = useStore();
 
-    let bilanz = storeToRefs(store).bilanzen;
-    await store.loadBilanzen();
-    let bilanzValue = bilanz._rawValue[0];
-    console.log(bilanzValue);
+let bilanz = storeToRefs(store).bilanzen;
+await store.loadBilanzen();
+let bilanzValue = bilanz._rawValue[0];
+console.log(bilanzValue);
 
-    let guv = storeToRefs(store).GuVs;
-    await store.loadGuVs();
-    let guvValue = guv._rawValue[0];
-    console.log(guvValue);
+let guv = storeToRefs(store).GuVs;
+await store.loadGuVs();
+let guvValue = guv._rawValue[0];
+console.log(guvValue);
 
-    let props = ["id", "created_at"];
-    props.forEach(prop => {
-        delete bilanzValue[prop];
-        delete guvValue[prop];
-    });
-
+let props = ["id", "created_at"];
+props.forEach((prop) => {
+  delete bilanzValue[prop];
+  delete guvValue[prop];
+});
 </script>
 
 <template>
-<div class="flex flex-row gap-x-10">
+  <div class="flex flex-row gap-x-10">
     <table class="border border-slate-500">
-        <thead>
-            <tr class="border-slate-500 font-bold text-center">
-                Bilanz
-            </tr>
-            <tr>
-                <th class="border border-slate-500">Bezeichnung</th>
-                <th class="border border-slate-500">Betrag</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(value, key) in bilanzValue">
-                <td class="border border-slate-500">{{ key }}</td>
-                <td class="border border-slate-500">
-                    <p>€</p>
-                    <p class="text-end">{{ value.toLocaleString("de-DE", {minimumFractionDigits: 2})}}</p>
-                </td>
-            </tr>
-        </tbody>
+      <thead>
+        <tr class="border-slate-500 font-bold text-center">
+          Bilanz
+        </tr>
+        <tr>
+          <th class="border border-slate-500">Bezeichnung</th>
+          <th class="border border-slate-500">Betrag</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(value, key) in bilanzValue">
+          <td class="border border-slate-500">{{ key }}</td>
+          <td class="border border-slate-500">
+            <div class="flex justify-between">
+              <div>€</div>
+              <div>
+                {{
+                  value.toLocaleString("de-DE", { minimumFractionDigits: 2 })
+                }}
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
 
     <table class="border border-slate-500">
-        <thead class="border border-slate-500">
-            <tr class="border border-slate-500 font-bold text-center">
-                GuV
-            </tr>
-            <tr>
-                <th class="border border-slate-500">Bezeichnung</th>
-                <th class="border border-slate-500">Betrag</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(value, key) in guvValue">
-                <td class="border border-slate-500">{{ key }}</td>
-                <td class="text-right border border-slate-500"> € {{ value.toLocaleString("de-DE", {minimumFractionDigits: 2}) }}</td>
-            </tr>
-        </tbody>
+      <thead class="border border-slate-500">
+        <tr class="border border-slate-500 font-bold text-center">
+          GuV
+        </tr>
+        <tr>
+          <th class="border border-slate-500">Bezeichnung</th>
+          <th class="border border-slate-500">Betrag</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(value, key) in guvValue">
+          <td class="border border-slate-500">{{ key }}</td>
+          <td class="border border-slate-500">
+            <div class="flex justify-between">
+              <div>€</div>
+              <div>
+                {{
+                  value.toLocaleString("de-DE", { minimumFractionDigits: 2 })
+                }}
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
-</div>
-    
-
-
+  </div>
 </template>
 
 <style>
-*{
-  
+* {
 }
-p{
-    display: inline-block;
+p {
+  display: inline-block;
 }
 </style>
