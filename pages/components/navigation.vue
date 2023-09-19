3b150fe522039c439799c4aebd0c6115a5f0f13c
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-09-19 21:03:03 +0200

    switched absolute to relative links

--- pages/components/navigation.vue
+++ pages/components/navigation.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="bg-slate-500 hover:bg-slate-400 flex justify-evenly">
-    
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/"
+      href="/"
       >New Game</a
     >
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 bg-slate-500 font-bold py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/bilanzen"
+      href="/bilanzen"
       >Bilanzen</a
     >
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/settings"
+      href="/settings"
       >Settings</a
     >
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/help"
+      href="/help"
       >Help</a
     >
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/aboutus"
+      href="/aboutus"
       >About Us</a
     >
     <a
       class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-2 px-9 text-white text-center flex-grow"
-      href="http://localhost:3000/profile"
+      href="/profile"
       >Profile</a
     >
   </div>
--- pages/gamescreen.vue
+++ pages/gamescreen.vue
@@ -1,26 +1,33 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div><a href="http://localhost:3000/bilanzen">Bilanzen</a></div>
-    <div class="flex flex-col justify-center items-center h-screen">
-      <div class="flex">
-        <button class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center mr-12 rounded-full mb-10">
-          Choice 1
-        </button>
-        <button class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center rounded-full mb-10">
-          Choice 2
-        </button>
-      </div>
-      <div class="flex mt-4">
-        <button class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center mr-12 rounded-full mb-10">
-          Choice 3
-        </button>
-        <button class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center rounded-full mb-10">
-          Choice 4
-        </button>
-      </div>
+  <div><a href="/bilanzen">Bilanzen</a></div>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div class="flex">
+      <button
+        class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center mr-12 rounded-full mb-10"
+      >
+        Choice 1
+      </button>
+      <button
+        class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center rounded-full mb-10"
+      >
+        Choice 2
+      </button>
     </div>
+    <div class="flex mt-4">
+      <button
+        class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center mr-12 rounded-full mb-10"
+      >
+        Choice 3
+      </button>
+      <button
+        class="group hover:text-slate-600 hover:bg-slate-300 font-bold bg-slate-500 py-6 px-12 text-white text-center rounded-full mb-10"
+      >
+        Choice 4
+      </button>
+    </div>
+  </div>
 </template>
 
 <style>
