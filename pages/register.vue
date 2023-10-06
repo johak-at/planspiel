afe78981c73d1a98e1381a59a3fba948551f9b00
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2023-10-06 11:40:07 +0200

    Nur Layout

--- pages/register.vue
+++ pages/register.vue
@@ -22,6 +22,8 @@ async function signUp() {
   }
 }
 
+//functon to load uid from supabase current user
+
 // async function loadUser() {
 //   const res = await supabase.from("User").select("*");
 //   user.value = res.data;
@@ -62,41 +64,66 @@ async function signUp() {
 <template>
   <div class="h-screen flex">
     <div class="flex-1 flex items-center justify-center bg-[#f1f1f1]">
-      <form @submit.prevent="signUp"
-        class="flex flex-col px-16 py-8 rounded-md shadow-lg max-w-screen-sm w-full bg-white">
+      <form
+        @submit.prevent="signUp"
+        class="flex flex-col px-16 py-8 rounded-md shadow-lg max-w-screen-sm w-full bg-white"
+      >
         <div class="flex items-center justify-center mb-4">
           <img class="w-20" src="../assets/Platzhalter.png" alt="Logo" />
         </div>
         <h2 class="text-2xl font-semibold mb-4 text-slate-700">Register</h2>
 
         <!-- Display success message -->
-        <div v-if="successMsg" class="bg-green-200 p-2 rounded-md text-green-700 mb-4">
+        <div
+          v-if="successMsg"
+          class="bg-green-200 p-2 rounded-md text-green-700 mb-4"
+        >
           {{ successMsg }}
         </div>
 
         <!-- Display error message -->
-        <div v-if="errorMsg" class="bg-red-200 p-2 rounded-md text-red-700 mb-4">
+        <div
+          v-if="errorMsg"
+          class="bg-red-200 p-2 rounded-md text-red-700 mb-4"
+        >
           {{ errorMsg }}
         </div>
 
         <div class="mb-4">
-          <input v-model="email" type="email" id="email" name="email" placeholder="Email"
-            class="mt-1 p-2 w-full border rounded-md bg-white text-black" />
+          <input
+            v-model="email"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            class="mt-1 p-2 w-full border rounded-md bg-white text-black"
+          />
         </div>
 
         <div class="mb-4">
-          <input v-model="password" type="password" id="password" name="password" placeholder="Password"
-            class="mt-1 p-2 w-full border rounded-md bg-white text-black" />
+          <input
+            v-model="password"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            class="mt-1 p-2 w-full border rounded-md bg-white text-black"
+          />
         </div>
 
-        <button type="submit" class="bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 w-20">
+        <button
+          type="submit"
+          class="bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 w-20"
+        >
           Register
         </button>
         <!-- make a text in the middle saying Already have an account? Login -->
         <div class="flex items-center justify-center mt-4 text-black">
           <p class="text-sm text-black font-semibold">
             Already have an account?
-            <a href="/login" class="text-black font-bold hover:text-blue-600">Login</a>
+            <a href="/login" class="text-black font-bold hover:text-blue-600"
+              >Login</a
+            >
           </p>
         </div>
       </form>
